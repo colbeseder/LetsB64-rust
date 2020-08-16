5669a65af111da4d3e30df0f72eaecb5be97025e
Bryan <bryanjappleby@gmail.com>
2020-08-16 22:09:18 +0300

    Moved to tests to separate JSON file

--- sanity.py
+++ sanity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import subprocess
+import subprocess, json
 
 def cmd(commands):
     r = ""
@@ -11,38 +11,8 @@ def cmd(commands):
         return 1, r
 
 # bash, expected code, expected output
-tests = [
-    #Compile
-    [["rustc", "letsB64.rs"], 0, ""],
-    # No args
-    [["./letsB64"],           1, ""],
-    # Not enough args
-    [["./letsB64", "e"],      1, ""],
-    # Command doesn't exist
-    [["./letsB64", "f"],      1, ""],
-    # Wrong length for decode
-    [["./letsB64", "d", "a="],1, ""],
-
-    #Encoding
-    [["./letsB64", "encode", ""                         ], 0, ""],
-    [["./letsB64", "e",      ""                         ], 0, ""],
-    [["./letsB64", "e",      "a"                        ], 0, "YQ=="],
-    [["./letsB64", "e",      "be"                       ], 0, "YmU="],
-    [["./letsB64", "e",      "see"                      ], 0, "c2Vl"],
-    [["./letsB64", "e",      "ddee"                     ], 0, "ZGRlZQ=="],
-    [["./letsB64", "e",      "welcome!"                 ], 0, "d2VsY29tZSE="],
-    [["./letsB64", "e",      "I'm feeling great"        ], 0, "SSdtIGZlZWxpbmcgZ3JlYXQ="],
-
-    #Decoding
-    [["./letsB64", "decode", ""                         ], 0, ""],
-    [["./letsB64", "d",      ""                         ], 0, ""],
-    [["./letsB64", "d",      "YQ=="                     ], 0, "a"],
-    [["./letsB64", "d",      "YmU="                     ], 0, "be"],
-    [["./letsB64", "d",      "c2Vl"                     ], 0, "see"],
-    [["./letsB64", "d",      "ZGRlZQ=="                 ], 0, "ddee"],
-    [["./letsB64", "d",      "d2VsY29tZSE="             ], 0, "welcome!"],
-    [["./letsB64", "d",      "SSdtIGZlZWxpbmcgZ3JlYXQ=" ], 0, "I'm feeling great"]
-]
+with open('tests.json') as json_file:
+    tests = json.load(json_file)
 
 # Delete old binary to ensure that we're testing latest code
 cmd(["rm", "letsB64"])
